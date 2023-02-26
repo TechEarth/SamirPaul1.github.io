4b1a945c4d80d804a9325bc3d4e509a34668526d
Samir Paul <samir.paul.personal@egmail.com>
2023-02-26 17:15:31 +0530

    revert back to Chirpy v5.3.2

--- _javascript/utils/img-extra.js
+++ _javascript/utils/img-extra.js
@@ -1,13 +1,33 @@
 /**
- * Set up image stuff
+ Lazy load images (https://github.com/ApoorvSaxena/lozad.js)
+ and popup when clicked (https://github.com/dimsemenov/Magnific-Popup)
  */
 
-(function() {
-    if ($('#core-wrapper img[data-src]') <= 0) {
+$(function () {
+    const IMG_SCOPE = '#main > div.row:first-child > div:first-child';
+
+    if ($(`${IMG_SCOPE} img`).length <= 0) {
         return;
     }
 
-    /* See: <https://github.com/dimsemenov/Magnific-Popup> */
+    /* lazy loading */
+
+    const imgList = document.querySelectorAll(`${IMG_SCOPE} img[data-src]`);
+    const observer = lozad(imgList);
+    observer.observe();
+
+    /* popup */
+
+    $(`${IMG_SCOPE} p > img[data-src], ${IMG_SCOPE} img[data-src].preview-img`).each(
+        function () {
+            let nextTag = $(this).next();
+            const title = nextTag.prop('tagName') === 'EM' ? nextTag.text() : '';
+            const src = $(this).attr('data-src'); // created by lozad.js
+
+            $(this).wrap(`<a href="${src}" title="${title}" class="popup"></a>`);
+        }
+    );
+
     $('.popup').magnificPopup({
         type: 'image',
         closeOnContentClick: true,
@@ -19,10 +39,8 @@
         }
     });
 
-    /* Stop shimmer when image loaded */
-    document.addEventListener('lazyloaded', function(e) {
-        const $img = $(e.target);
-        $img.parent().removeClass('shimmer');
-    });
+    /* markup the image links */
 
-})();
+    $(`${IMG_SCOPE} a`).has('img').addClass('img-link');
+
+});
